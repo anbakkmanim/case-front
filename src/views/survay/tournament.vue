9b90d417139e8ca91326b9406fa6c1979948901d
서상희 <tbvjaos510@naver.com>
2019-07-05 12:09:35 +0900

    [ADD] 토너먼트 시작

--- src/views/survay/tournament.vue
+++ src/views/survay/tournament.vue
@@ -4,7 +4,13 @@
     class="border border-solid border-gray-400 rounded relative overflow-hidden"
   >
     <case-form-wizard title="토너먼트 생성" subtitle="">
-      <tab-content title="토너먼트 기본 설정"> </tab-content>
+      <tab-content title="토너먼트 기본 설정">
+        <div class="vx-row">
+          <div class="vx-col sm:w-1/2 w-full mb-2">
+            <vs-input class="w-full" label="토너먼트 제목" v-model="title" />
+          </div>
+        </div>
+      </tab-content>
 
       <tab-content title="토너먼트 항목 생성"> </tab-content>
 
@@ -17,6 +23,11 @@
 import CaseFormWizard from '@/components/form/CaseFormWizard.vue';
 
 export default {
+  data() {
+    return {
+      title: '',
+    };
+  },
   methods: {
     onComplate() {},
   },
