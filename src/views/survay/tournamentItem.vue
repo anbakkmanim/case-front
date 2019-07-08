9abd440a015c1c607aa6aa587ed44783b7c493b7
서상희 <tbvjaos510@naver.com>
2019-07-08 15:51:02 +0900

    [ADD] 토너먼트 제작중...

---
+++ src/views/survay/tournamentItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <vs-card>
+    <div slot="header">
+      <h3>{{ title }}</h3>
+    </div>
+    <div slot="media">
+      <img :src="image" />
+    </div>
+  </vs-card>
+</template>
+
+<script>
+export default {
+  name: 'tournament-item',
+  props: ['title', 'image'],
+};
+</script>
+
+<style>
+</style>
