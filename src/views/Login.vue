01d05d3eed07c93dc21cad61e9b6c9db1911d842
남현욱 <37534735+hw0k@users.noreply.github.com>
2019-07-08 20:15:14 +0900

    [FIX] Bugfix :)

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-screen flex w-full bg-img vx-row no-gutter items-center justify-center" id="page-login">
     <div class="vx-col sm:w-1/2 md:w-1/2 lg:w-3/4 xl:w-3/5 sm:m-0 m-4">
-      <vx-card>
+      <case-card>
         <div slot="no-body" class="full-page-bg-color">
           <div class="vx-row no-gutter justify-center items-center">
             <div class="vx-col hidden lg:block lg:w-1/2">
@@ -49,7 +49,7 @@
             </div>
           </div>
         </div>
-      </vx-card>
+      </case-card>
     </div>
   </div>
 </template>
