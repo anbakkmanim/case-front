f99891fff1098970f6152f57df923deaea4e21f5
김민기 <alsrltt@nate.com>
2019-07-09 15:59:37 +0900

    [ADD] selection 모양 완성

---
+++ src/views/survey/selectionItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <ul class="centerx">
+      <li v-for="i in Count" :key="i">
+        <vs-radio class="ml-4" v-model="select" vs-value=i>
+          <vs-input
+                  class="w-full p-2"
+                  name=i
+                  v-model="selection[i]"
+                  v-validate="'required'"
+                />
+          </vs-radio>
+      </li>
+    </ul>
+
+    <vs-button class="ml-4" type="line" @click="addSelection">보기 추가</vs-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      title: '',
+      images: [],
+      isSet: true,
+      select: '',
+      selection: [],
+      Count: 2,
+    };
+  },
+  methods: {
+    addSelection() {
+      if (this.Count < 5) {
+        this.Count++;
+      }
+    },
+  },
+  name: 'selection-item',
+  props: ['idx'],
+};
+</script>
+
+<style>
+</style>
