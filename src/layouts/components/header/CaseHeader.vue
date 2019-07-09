506daccaba7e5364dc8943f9de258b5153cff765
서상희 <tbvjaos510@naver.com>
2019-07-09 20:28:28 +0900

    [UPDATE] 헤더 사용자 뺴고 전부 삭제

--- src/layouts/components/header/CaseHeader.vue
+++ src/layouts/components/header/CaseHeader.vue
@@ -1,24 +1,30 @@
 <template>
-<div class="relative">
-  <div class="vx-navbar-wrapper">
-    <vs-navbar class="vx-navbar navbar-custom" :color="navbarColor" :class="classObj">
+  <div class="relative">
+    <div class="vx-navbar-wrapper">
+      <vs-navbar
+        class="vx-navbar navbar-custom"
+        :color="navbarColor"
+        :class="classObj"
+      >
+        <!-- SM - OPEN SIDEBAR BUTTON -->
+        <feather-icon
+          class="sm:inline-flex xl:hidden cursor-pointer mr-1"
+          icon="MenuIcon"
+          @click.stop="showSidebar"
+        ></feather-icon>
 
-      <!-- SM - OPEN SIDEBAR BUTTON -->
-      <feather-icon class="sm:inline-flex xl:hidden cursor-pointer mr-1" icon="MenuIcon" @click.stop="showSidebar"></feather-icon>
-
-      <template v-if="breakpoint != 'md'">
+        <!-- <template v-if="breakpoint != 'md'">
         <div class="bookmark-container">
           <feather-icon icon="StarIcon" :svgClasses="['stoke-current text-warning', {'text-white': navbarColor != '#fff'}]" class="cursor-pointer p-2" @click="showBookmarkPagesDropdown = !showBookmarkPagesDropdown" />  <div v-click-outside="outside" class="absolute bookmark-list w-1/3 xl:w-1/4 mt-4" v-if="showBookmarkPagesDropdown">
             <case-auto-suggest :autoFocus="true" :data="navbarSearchAndPinList" @selected="selected" @actionClicked="actionClicked" inputClassses="w-full" show-action show-pinned background-overlay></case-auto-suggest>
           </div>
         </div>
-      </template>
+      </template> -->
 
+        <vs-spacer></vs-spacer>
 
-      <vs-spacer></vs-spacer>
-
-      <!-- NOTIFICATIONS -->
-      <vs-dropdown vs-custom-content vs-trigger-click>
+        <!-- NOTIFICATIONS -->
+        <!-- <vs-dropdown vs-custom-content vs-trigger-click>
         <feather-icon icon="BellIcon" class="cursor-pointer mx-6 mt-1" :badge="unreadNotifications.length"></feather-icon>
         <vs-dropdown-menu class="notification-dropdown">
           <div class="notification-top text-center p-5 bg-primary text-white">
@@ -43,46 +49,98 @@
           </ul>
           </VuePerfectScrollbar>
         </vs-dropdown-menu>
-      </vs-dropdown>
+      </vs-dropdown> -->
 
-      <!-- SEARCHBAR -->
-      <div class="search-full-container w-full h-full absolute pin-l rounded-lg" :class="{'flex': showFullSearch}" v-show="showFullSearch">
+        <!-- SEARCHBAR -->
+        <!-- <div class="search-full-container w-full h-full absolute pin-l rounded-lg" :class="{'flex': showFullSearch}" v-show="showFullSearch">
         <case-auto-suggest :autoFocus="showFullSearch" :data="navbarSearchAndPinList" @selected="selected" ref="navbarSearch" @closeSearchbar="showFullSearch = false" placeholder="Search..." class="w-full" inputClassses="w-full input-no-border no-icon-border" icon="SearchIcon" background-overlay></case-auto-suggest>
         <div class="absolute pin-r h-full z-50">
           <feather-icon icon="XIcon" class="px-4 cursor-pointer h-full close-search-icon" @click="showFullSearch = false"></feather-icon>
         </div>
-      </div>
-      <feather-icon icon="SearchIcon" @click="showFullSearch = true" class="cursor-pointer"></feather-icon>
+      </div> -->
+        <!-- <feather-icon icon="SearchIcon" @click="showFullSearch = true" class="cursor-pointer"></feather-icon> -->
 
-      <!-- USER META -->
-      <div class="the-navbar__user-meta flex items-center sm:ml-5 ml-2">
-        <div class="text-right leading-tight hidden sm:block">
-          <p class="font-semibold">John Doe</p>
-          <small>Available</small>
+        <!-- USER META -->
+        <div class="the-navbar__user-meta flex items-center sm:ml-5 ml-2">
+          <div class="text-right leading-tight hidden sm:block">
+            <p class="font-semibold">John Doe</p>
+            <small>Available</small>
+          </div>
+          <vs-dropdown vs-custom-content vs-trigger-click>
+            <div class="con-img ml-3">
+              <img
+                :src="
+                  require(`@/assets/images/portrait/small/${activeUserInfo.img}`)
+                "
+                alt=""
+                width="40"
+                height="40"
+                class="rounded-full shadow-md cursor-pointer block"
+              />
+            </div>
+            <vs-dropdown-menu>
+              <ul style="min-width: 9rem">
+                <li
+                  class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"
+                  @click="$router.push('/pages/profile')"
+                >
+                  <feather-icon
+                    icon="UserIcon"
+                    svgClasses="w-4 h-4"
+                  ></feather-icon>
+                  <span class="ml-2">Profile</span>
+                </li>
+                <li
+                  class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"
+                  @click="$router.push('/apps/email')"
+                >
+                  <feather-icon
+                    icon="MailIcon"
+                    svgClasses="w-4 h-4"
+                  ></feather-icon>
+                  <span class="ml-2">Inbox</span>
+                </li>
+                <li
+                  class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"
+                  @click="$router.push('/apps/todo')"
+                >
+                  <feather-icon
+                    icon="CheckSquareIcon"
+                    svgClasses="w-4 h-4"
+                  ></feather-icon>
+                  <span class="ml-2">Tasks</span>
+                </li>
+                <li
+                  class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"
+                  @click="$router.push('/apps/chat')"
+                >
+                  <feather-icon
+                    icon="MessageSquareIcon"
+                    svgClasses="w-4 h-4"
+                  ></feather-icon>
+                  <span class="ml-2">Chat</span>
+                </li>
+                <vs-divider class="m-1"></vs-divider>
+                <li
+                  class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"
+                  @click="$router.push('/pages/login')"
+                >
+                  <feather-icon
+                    icon="LogOutIcon"
+                    svgClasses="w-4 h-4"
+                  ></feather-icon>
+                  <span class="ml-2">Logout</span>
+                </li>
+              </ul>
+            </vs-dropdown-menu>
+          </vs-dropdown>
         </div>
-        <vs-dropdown vs-custom-content vs-trigger-click>
-          <div class="con-img ml-3"><img :src="require(`@/assets/images/portrait/small/${activeUserInfo.img}`)" alt="" width="40" height="40" class="rounded-full shadow-md cursor-pointer block"></div>
-          <vs-dropdown-menu>
-            <ul style="min-width: 9rem">
-              <li class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white" @click="$router.push('/pages/profile')"><feather-icon icon="UserIcon" svgClasses="w-4 h-4"></feather-icon> <span class="ml-2">Profile</span></li>
-              <li class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white" @click="$router.push('/apps/email')"><feather-icon icon="MailIcon" svgClasses="w-4 h-4"></feather-icon> <span class="ml-2">Inbox</span></li>
-              <li class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white" @click="$router.push('/apps/todo')"><feather-icon icon="CheckSquareIcon" svgClasses="w-4 h-4"></feather-icon> <span class="ml-2">Tasks</span></li>
-              <li class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white" @click="$router.push('/apps/chat')"><feather-icon icon="MessageSquareIcon" svgClasses="w-4 h-4"></feather-icon> <span class="ml-2">Chat</span></li>
-              <vs-divider class="m-1"></vs-divider>
-              <li class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white" @click="$router.push('/pages/login')"><feather-icon icon="LogOutIcon" svgClasses="w-4 h-4"></feather-icon> <span class="ml-2">Logout</span></li>
-            </ul>
-          </vs-dropdown-menu>
-        </vs-dropdown>
-      </div>
-
-    </vs-navbar>
+      </vs-navbar>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import VuePerfectScrollbar from 'vue-perfect-scrollbar';
-import CaseAutoSuggest from '@/components/auto-suggest/AutoSuggest.vue';
 
 export default {
   name: 'case-header',
@@ -223,10 +281,6 @@ export default {
         el.__vueClickOutside__ = null;
       },
     },
-  },
-  components: {
-    CaseAutoSuggest,
-    VuePerfectScrollbar,
   },
 };
 </script>
