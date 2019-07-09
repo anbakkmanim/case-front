ac3e7951da25e769bf440e3bb025f9183b7823d9
nsh5438 <nsh5438@naver.com>
2019-07-09 18:00:17 +0900

    [UPDATE] 회원가입 UI 완성 및 프로필 수정

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -4,7 +4,7 @@
       <vx-card>
         <div slot="no-body" class="full-page-bg-color">
           <div class="vx-row">
-            <div class="vx-col hidden sm:hidden md:hidden lg:block lg:w-1/2 mx-auto self-center">
+            <div class="vx-col hidden sm:hidden md:hidden lg:block lg:w-1/2 mx-auto self-center  text-center">
               <img src="@/assets/images/pages/forgot-password.png" alt="login" class="mx-auto">
             </div>
             <div class="vx-col sm:w-full md:w-full lg:w-1/2 mx-auto self-center bg-white text-left">
